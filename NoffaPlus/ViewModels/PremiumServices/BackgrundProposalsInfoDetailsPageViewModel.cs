fcc5254535c0a736b28c73e2b51689c4e6582c44
gursewakinsan <insagursewakinsa@gmail.com>
2023-06-17 13:02:25 +0530

    working

--- NoffaPlus/ViewModels/PremiumServices/BackgrundProposalsInfoDetailsPageViewModel.cs
+++ NoffaPlus/ViewModels/PremiumServices/BackgrundProposalsInfoDetailsPageViewModel.cs
@@ -4,28 +4,15 @@ using NoffaPlus.Service;
 using System.Windows.Input;
 using NoffaPlus.Interfaces;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace NoffaPlus.ViewModels
 {
-    public class ProposalsInfoDetailsPageViewModel : BaseViewModel
+    public class BackgrundProposalsInfoDetailsPageViewModel : BaseViewModel
     {
         #region Constructor.
-        public ProposalsInfoDetailsPageViewModel(INavigation navigation)
+        public BackgrundProposalsInfoDetailsPageViewModel(INavigation navigation)
         {
             Navigation = navigation;
-        }
-        #endregion
-
-        #region Backgrund Proposals Info Details Command.
-        private ICommand backgrundProposalsInfoDetailsCommand;
-        public ICommand BackgrundProposalsInfoDetailsCommand
-        {
-            get => backgrundProposalsInfoDetailsCommand ?? (backgrundProposalsInfoDetailsCommand = new Command(async () => await ExecuteBackgrundProposalsInfoDetailsCommand()));
-        }
-        private async Task ExecuteBackgrundProposalsInfoDetailsCommand()
-        {
-            await Navigation.PushAsync(new Views.PremiumServices.BackgrundProposalsInfoDetailsPage(ProposalsInfoDetails, ProposalsDates));
         }
         #endregion
 
---
+++ NoffaPlus/Views/PremiumServices/BackgrundProposalsInfoDetailsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NoffaPlus.ViewModels;
+
+namespace NoffaPlus.Views.PremiumServices
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BackgrundProposalsInfoDetailsPage : ContentPage
+    {
+        BackgrundProposalsInfoDetailsPageViewModel viewModel;
+        public BackgrundProposalsInfoDetailsPage(Models.EmployeeProfessionalServiceProposalsResponse proposalsInfo, Models.EmployeeProfessionalServiceProposalsDatesResponse dates)
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
+            BindingContext = viewModel = new BackgrundProposalsInfoDetailsPageViewModel(this.Navigation);
+            viewModel.ProposalsInfoDetails = proposalsInfo;
+            viewModel.ProposalsDates = dates;
+        }
+    }
+}
