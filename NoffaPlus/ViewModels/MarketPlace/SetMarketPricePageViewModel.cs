ca107950ab32b8cb469ac340a3fd2a2114a67d87
gursewakinsan <insagursewakinsa@gmail.com>
2023-12-16 23:33:36 +0530

    working on it

---
+++ NoffaPlus/ViewModels/MarketPlace/SetMarketPricePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using NoffaPlus.Service;
+using System.Windows.Input;
+using NoffaPlus.Interfaces;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace NoffaPlus.ViewModels
+{
+    public class SetMarketPricePageViewModel : BaseViewModel
+    {
+        #region Constructor.
+        public SetMarketPricePageViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+    }
+}
---
+++ NoffaPlus/Views/MarketPlace/SetMarketPricePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NoffaPlus.ViewModels;
+
+namespace NoffaPlus.Views.MarketPlace
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SetMarketPricePage : ContentPage
+    {
+        SetMarketPricePageViewModel ViewModel;
+        public SetMarketPricePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
+            BindingContext = ViewModel = new SetMarketPricePageViewModel(this.Navigation);
+        }
+    }
+}
