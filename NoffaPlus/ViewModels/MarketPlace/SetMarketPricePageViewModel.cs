c02397431e9173ab35b913b825d78d2e77f7a027
gursewakinsan <insagursewakinsa@gmail.com>
2023-12-17 18:37:22 +0530

    working..

--- NoffaPlus/ViewModels/MarketPlace/SetMarketPricePageViewModel.cs
+++ NoffaPlus/ViewModels/MarketPlace/SetMarketPricePageViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using NoffaPlus.Interfaces;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using ZXing.QrCode.Internal;
 
 namespace NoffaPlus.ViewModels
 {
@@ -64,11 +65,13 @@ namespace NoffaPlus.ViewModels
         private ICommand isBookableServiceCommand;
         public ICommand IsBookableServiceCommand
         {
-            get => isBookableServiceCommand ?? (isBookableServiceCommand = new Command( () =>  ExecuteIsBookableServiceCommand()));
+            get => isBookableServiceCommand ?? (isBookableServiceCommand = new Command(() => ExecuteIsBookableServiceCommand()));
         }
         private void ExecuteIsBookableServiceCommand()
         {
             IsBookableService = !IsBookableService;
+            if(IsBookableService) 
+                SelectedBookingTypeModel = BookingTypeModel[0];
         }
         #endregion
 
@@ -81,6 +84,32 @@ namespace NoffaPlus.ViewModels
         private void ExecuteIsRecurringEventCommand()
         {
             IsRecurringEvent = !IsRecurringEvent;
+            if (IsRecurringEvent)
+            {
+                IsMondayControl = true;
+                IsTuesdayControl = true;
+                IsWednesdayControl = true;
+                IsThursdayControl = true;
+                IsFridayControl = true;
+                IsSaturdayControl = true;
+                IsSundayControl = true;
+                IsPricePerX = true;
+                IsEventDateControl = false;
+                IsEventStartTimeControl = false;
+            }
+            else
+            {
+                IsMondayControl = false;
+                IsTuesdayControl = false;
+                IsWednesdayControl = false;
+                IsThursdayControl = false;
+                IsFridayControl = false;
+                IsSaturdayControl = false;
+                IsSundayControl = false;
+                IsPricePerX = true;
+                IsEventDateControl = true;
+                IsEventStartTimeControl = true;
+            }
         }
         #endregion
 
@@ -167,7 +196,7 @@ namespace NoffaPlus.ViewModels
             }
         }
 
-        public bool isBookableService;
+        public bool isBookableService = false;
         public bool IsBookableService
         {
             get => isBookableService;
@@ -175,6 +204,7 @@ namespace NoffaPlus.ViewModels
             {
                 isBookableService = value;
                 OnPropertyChanged("IsBookableService");
+                ShowHideOnBookableService(isBookableService);
             }
         }
 
@@ -351,6 +381,7 @@ namespace NoffaPlus.ViewModels
             {
                 selectedBookingTypeModel = value;
                 OnPropertyChanged("SelectedBookingTypeModel");
+                ShowHideOnBookingType(selectedBookingTypeModel.BookingType);
             }
         }
 
@@ -373,9 +404,431 @@ namespace NoffaPlus.ViewModels
             {
                 selectedSharedTypeModel = value;
                 OnPropertyChanged("SelectedSharedTypeModel");
+                OnSharedTypeSelected(selectedSharedTypeModel.SharedType);
+            }
+        }
+
+        private bool isPricePerX;
+        public bool IsPricePerX
+        {
+            get => isPricePerX;
+            set
+            {
+                isPricePerX = value;
+                OnPropertyChanged("IsPricePerX");
+            }
+        }
+
+        private bool isPricePerY;
+        public bool IsPricePerY
+        {
+            get => isPricePerY;
+            set
+            {
+                isPricePerY = value;
+                OnPropertyChanged("IsPricePerY");
+            }
+        }
+
+        private bool isAtCustomerLocation = true;
+        public bool IsAtCustomerLocation
+        {
+            get => isAtCustomerLocation;
+            set
+            {
+                isAtCustomerLocation = value;
+                OnPropertyChanged("IsAtCustomerLocation");
+            }
+        }
+
+        private bool isMoreEventsUponRequest = true;
+        public bool IsMoreEventsUponRequest
+        {
+            get => isMoreEventsUponRequest;
+            set
+            {
+                isMoreEventsUponRequest = value;
+                OnPropertyChanged("IsMoreEventsUponRequest");
+            }
+        }
+
+        private bool isBookingTypeControl;
+        public bool IsBookingTypeControl
+        {
+            get => isBookingTypeControl;
+            set
+            {
+                isBookingTypeControl = value;
+                OnPropertyChanged("IsBookingTypeControl");
+            }
+        }
+
+        private bool isSharedTypeControl;
+        public bool IsSharedTypeControl
+        {
+            get => isSharedTypeControl;
+            set
+            {
+                isSharedTypeControl = value;
+                OnPropertyChanged("IsSharedTypeControl");
+            }
+        }
+
+        private bool isRecurringEventControl;
+        public bool IsRecurringEventControl
+        {
+            get => isRecurringEventControl;
+            set
+            {
+                isRecurringEventControl = value;
+                OnPropertyChanged("IsRecurringEventControl");
+                ShowHideOnRecurringEventControl(isRecurringEventControl);
+            }
+        }
+
+        void ShowHideOnRecurringEventControl(bool status)
+        {
+            if(status) 
+            {
+                IsMondayControl = true;
+                IsTuesdayControl = true;
+                IsWednesdayControl = true;
+                IsThursdayControl = true;
+                IsFridayControl = true;
+                IsSaturdayControl = true;
+                IsSundayControl = true;
+                IsEventDateControl = false;
+                IsEventStartTimeControl = false;
+            }
+            else 
+            {
+                IsMondayControl = false;
+                IsTuesdayControl = false;
+                IsWednesdayControl = false;
+                IsThursdayControl = false;
+                IsFridayControl = false;
+                IsSaturdayControl = false;
+                IsSundayControl = false;
+                if (SelectedSharedTypeModel !=null && SelectedSharedTypeModel.SharedType.Equals("Private"))
+                {
+                    IsEventDateControl = true;
+                    IsEventStartTimeControl = true;
+                }
+            }
+        }
+
+        private bool isEventDateControl;
+        public bool IsEventDateControl
+        {
+            get => isEventDateControl;
+            set
+            {
+                isEventDateControl = value;
+                OnPropertyChanged("IsEventDateControl");
+            }
+        }
+
+        private bool isEventStartTimeControl;
+        public bool IsEventStartTimeControl
+        {
+            get => isEventStartTimeControl;
+            set
+            {
+                isEventStartTimeControl = value;
+                OnPropertyChanged("IsEventStartTimeControl");
+            }
+        }
+
+        private bool isMondayControl;
+        public bool IsMondayControl
+        {
+            get => isMondayControl;
+            set
+            {
+                isMondayControl = value;
+                OnPropertyChanged("IsMondayControl");
+            }
+        }
+
+        private bool isTuesdayControl;
+        public bool IsTuesdayControl
+        {
+            get => isTuesdayControl;
+            set
+            {
+                isTuesdayControl = value;
+                OnPropertyChanged("IsTuesdayControl");
+            }
+        }
+
+        private bool isWednesdayControl;
+        public bool IsWednesdayControl
+        {
+            get => isWednesdayControl;
+            set
+            {
+                isWednesdayControl = value;
+                OnPropertyChanged("IsWednesdayControl");
+            }
+        }
+
+        private bool isThursdayControl;
+        public bool IsThursdayControl
+        {
+            get => isThursdayControl;
+            set
+            {
+                isThursdayControl = value;
+                OnPropertyChanged("IsThursdayControl");
+            }
+        }
+
+        private bool isFridayControl;
+        public bool IsFridayControl
+        {
+            get => isFridayControl;
+            set
+            {
+                isFridayControl = value;
+                OnPropertyChanged("IsFridayControl");
+            }
+        }
+
+        private bool isSaturdayControl;
+        public bool IsSaturdayControl
+        {
+            get => isSaturdayControl;
+            set
+            {
+                isSaturdayControl = value;
+                OnPropertyChanged("IsSaturdayControl");
+            }
+        }
+
+        private bool isSundayControl;
+        public bool IsSundayControl
+        {
+            get => isSundayControl;
+            set
+            {
+                isSundayControl = value;
+                OnPropertyChanged("IsSundayControl");
+            }
+        }
+
+        private bool isAtCustomerLocationControl;
+        public bool IsAtCustomerLocationControl
+        {
+            get => isAtCustomerLocationControl;
+            set
+            {
+                isAtCustomerLocationControl = value;
+                OnPropertyChanged("IsAtCustomerLocationControl");
+            }
+        }
+
+        private bool isTaxControl;
+        public bool IsTaxControl
+        {
+            get => isTaxControl;
+            set
+            {
+                isTaxControl = value;
+                OnPropertyChanged("IsTaxControl");
+            }
+        }
+
+        private bool isMoreEventsUponRequestControl;
+        public bool IsMoreEventsUponRequestControl
+        {
+            get => isMoreEventsUponRequestControl;
+            set
+            {
+                isMoreEventsUponRequestControl = value;
+                OnPropertyChanged("IsMoreEventsUponRequestControl");
+            }
+        }
+
+        private bool isMinimumPersonRequiredControl;
+        public bool IsMinimumPersonRequiredControl
+        {
+            get => isMinimumPersonRequiredControl;
+            set
+            {
+                isMinimumPersonRequiredControl = value;
+                OnPropertyChanged("IsMinimumPersonRequiredControl");
+            }
+        }
+
+        private bool isRequestPeriodControl;
+        public bool IsRequestPeriodControl
+        {
+            get => isRequestPeriodControl;
+            set
+            {
+                isRequestPeriodControl = value;
+                OnPropertyChanged("IsRequestPeriodControl");
+            }
+        }
+
+        private bool isTimeControl;
+        public bool IsTimeControl
+        {
+            get => isTimeControl;
+            set
+            {
+                isTimeControl = value;
+                OnPropertyChanged("IsTimeControl");
+            }
+        }
+
+        private bool isExtraFeeApplicableControl;
+        public bool IsExtraFeeApplicableControl
+        {
+            get => isExtraFeeApplicableControl;
+            set
+            {
+                isExtraFeeApplicableControl = value;
+                OnPropertyChanged("IsExtraFeeApplicableControl");
+            }
+        }
+
+        private bool isExtraFeeControl;
+        public bool IsExtraFeeControl
+        {
+            get => isExtraFeeControl;
+            set
+            {
+                isExtraFeeControl = value;
+                OnPropertyChanged("IsExtraFeeControl");
             }
         }
         #endregion
+
+        void ShowHideOnBookableService(bool status)
+        {
+            if (!status)
+            {
+                IsBookingTypeControl = false;
+                IsSharedTypeControl = false;
+                IsRecurringEventControl = false;
+                IsMondayControl = false;
+                IsTuesdayControl = false;
+                IsWednesdayControl = false;
+                IsThursdayControl = false;
+                IsFridayControl = false;
+                IsSaturdayControl = false;
+                IsSundayControl = false;
+                IsPricePerX = false;
+                IsPricePerY = false;
+                IsAtCustomerLocationControl = false;
+                IsTaxControl = false;
+                IsMoreEventsUponRequestControl = false;
+                IsMinimumPersonRequiredControl = false;
+                IsRequestPeriodControl = false;
+                IsTimeControl = false;
+                IsExtraFeeApplicableControl = false;
+                IsExtraFeeControl = false;
+            }
+            else
+            {
+                IsBookingTypeControl = true;
+            }
+        }
+
+        void ShowHideOnBookingType(string bookingType)
+        {
+            if (bookingType.Equals("Shared"))
+            {
+                IsRecurringEvent = true;
+                IsSharedTypeControl = true;
+                IsRecurringEventControl = true;
+                IsMondayControl = true;
+                IsTuesdayControl = true;
+                IsWednesdayControl = true;
+                IsThursdayControl = true;
+                IsFridayControl = true;
+                IsSaturdayControl = true;
+                IsSundayControl = true;
+                IsPricePerX = true;
+                IsPricePerY = false;
+                IsAtCustomerLocationControl = false;
+                IsTaxControl = false;
+                IsMoreEventsUponRequestControl = false;
+                IsMinimumPersonRequiredControl = false;
+                IsRequestPeriodControl = false;
+                IsTimeControl = false;
+                IsExtraFeeApplicableControl = false;
+                IsExtraFeeControl = false;
+            }
+            else
+            {
+                IsRecurringEvent = false;
+                IsSharedTypeControl = false;
+                IsRecurringEventControl = false;
+                IsMondayControl = false;
+                IsTuesdayControl = false;
+                IsWednesdayControl = false;
+                IsThursdayControl = false;
+                IsFridayControl = false;
+                IsSaturdayControl = false;
+                IsSundayControl = false;
+                IsPricePerX = false;
+                IsPricePerY = false;
+                IsAtCustomerLocationControl = false;
+                IsTaxControl = false;
+                IsMoreEventsUponRequestControl = false;
+                IsMinimumPersonRequiredControl = false;
+                IsRequestPeriodControl = false;
+                IsTimeControl = false;
+                IsExtraFeeApplicableControl = false;
+                IsExtraFeeControl = false;
+            }
+        }
+
+        void OnSharedTypeSelected(string sharedType)
+        {
+            if (sharedType.Equals("Open"))
+            {
+                if (IsBookableService)
+                {
+                    IsRecurringEventControl = true;
+                    IsEventDateControl = false;
+                    IsEventStartTimeControl = false;
+                    ShowHideRecurringEvent(true);
+                    IsPricePerX = true;
+                }
+            }
+            else if (sharedType.Equals("Private"))
+            {
+                IsRecurringEventControl = false;
+                IsEventDateControl = false;
+                IsEventStartTimeControl = false;
+                IsPricePerX = false;
+                ShowHideRecurringEvent(false);
+            }
+            else if (sharedType.Equals("Both"))
+            {
+                IsEventDateControl = false;
+                IsEventStartTimeControl = false;
+                IsRecurringEventControl = false;
+                ShowHideRecurringEvent(true);
+                IsPricePerX = true;
+            }
+        }
+
+        void ShowHideRecurringEvent(bool status)
+        {
+            IsRecurringEvent = status;
+            IsMondayControl = status;
+            IsTuesdayControl = status;
+            IsWednesdayControl = status;
+            IsThursdayControl = status;
+            IsFridayControl = status;
+            IsSaturdayControl = status;
+            IsSundayControl = status;
+           // IsPricePerX = status;
+            
+        }
     }
 }
-
