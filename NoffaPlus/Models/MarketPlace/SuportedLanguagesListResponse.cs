3dda6d2dd452f9d063320268d88838b80877a3bf
apple <gursewaksinghs@gmail.com>
2023-12-19 21:19:09 +0530

    fixs in ios

--- NoffaPlus/Models/MarketPlace/SuportedLanguagesListResponse.cs
+++ NoffaPlus/Models/MarketPlace/SuportedLanguagesListResponse.cs
@@ -1,6 +1,23 @@
 ﻿namespace NoffaPlus.Models
 {
-    public class SuportedLanguagesListResponse
+    public class SuportedLanguagesListResponse : BaseModel
     {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "language_name")]
+        public string LanguageName { get; set; }
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "is_selected")]
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get => isSelected;
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
     }
 }
--- NoffaPlus/Views/MarketPlace/SuportedLanguagesListPage.xaml.cs
+++ NoffaPlus/Views/MarketPlace/SuportedLanguagesListPage.xaml.cs
@@ -20,5 +20,24 @@ namespace NoffaPlus.Views.MarketPlace
             base.OnAppearing();
             ViewModel.SuportedLanguagesListCommand.Execute(null);
         }
+
+        void OnButtonTapped(System.Object sender, System.EventArgs e)
+        {
+            Button control = sender as Button;
+            OnItemTapped(control.BindingContext as Models.SuportedLanguagesListResponse);
+        }
+
+        void OnLabelTapped(System.Object sender, System.EventArgs e)
+        {
+            Label control = sender as Label;
+            OnItemTapped(control.BindingContext as Models.SuportedLanguagesListResponse);
+        }
+
+        void OnItemTapped(Models.SuportedLanguagesListResponse suported)
+        {
+            suported.IsSelected = !suported.IsSelected;
+            ViewModel.LanguageId = suported.Id;
+            ViewModel.UpdateLanguageAvailableCommand.Execute(null);
+        }
     }
 }
